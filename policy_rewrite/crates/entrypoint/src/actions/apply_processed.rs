7874ff2c444cb9011f71c4527379aee29de6ca64
TeamDman <TeamDman9201@gmail.com>
2024-05-03 10:27:06 -0400

    don't fail entirely if tofu apply fails

--- policy_rewrite/crates/entrypoint/src/actions/apply_processed.rs
+++ policy_rewrite/crates/entrypoint/src/actions/apply_processed.rs
@@ -4,13 +4,17 @@ use anyhow::Result;
 use command::prelude::CommandBuilder;
 use command::prelude::CommandKind;
 use command::prelude::OutputBehaviour;
+use tracing::warn;
 pub async fn apply_processed() -> Result<()> {
     let processed_dir = PathBuf::from("ignore").join("processed");
-    CommandBuilder::new(CommandKind::Tofu)
+    let result = CommandBuilder::new(CommandKind::Tofu)
         .arg("apply")
         .use_run_dir(processed_dir)
         .use_output_behaviour(OutputBehaviour::Display)
         .run_raw()
-        .await?;
+        .await;
+    if let Err(e) = result {
+        warn!("Tofu apply failed, did you say no? {e:?}")
+    }
     Ok(())
 }
