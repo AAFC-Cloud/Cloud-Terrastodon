caa41c0e28293cbde8ae0ab85a090e66f721e183
TeamDman <TeamDman9201@gmail.com>
2024-06-04 11:53:18 -0400

    rename LocatableBlock to CodeReference

--- rust/crates/tofu_types/src/lib.rs
+++ rust/crates/tofu_types/src/lib.rs
@@ -1,6 +1,6 @@
 mod data;
 mod imports;
-mod locatable_block;
+mod code_reference;
 mod providers;
 mod resources;
 mod strings;
@@ -8,7 +8,7 @@ mod strings;
 pub mod prelude {
     pub use crate::data::*;
     pub use crate::imports::*;
-    pub use crate::locatable_block::*;
+    pub use crate::code_reference::*;
     pub use crate::providers::*;
     pub use crate::resources::*;
     pub use crate::strings::*;
