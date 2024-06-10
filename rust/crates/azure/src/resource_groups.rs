4a33ee3d0b0b4d56bbf00277d42812639ab1cd31
TeamDman <TeamDman9201@gmail.com>
2024-06-10 15:51:38 -0400

    fix resource group fetching using full sub id instead of short

--- rust/crates/azure/src/resource_groups.rs
+++ rust/crates/azure/src/resource_groups.rs
@@ -1,16 +1,16 @@
+use crate::prelude::gather_from_subscriptions;
 use anyhow::Result;
+use azure_types::prelude::Scope;
 use azure_types::prelude::Subscription;
 use azure_types::resource_groups::ResourceGroup;
 use command::prelude::CommandBuilder;
 use command::prelude::CommandKind;
 use indicatif::MultiProgress;
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::Arc;
 
-use crate::prelude::gather_from_subscriptions;
-use crate::prelude::SubscriptionMap;
-
-pub async fn fetch_all_resource_groups() -> Result<SubscriptionMap<Vec<ResourceGroup>>> {
+pub async fn fetch_all_resource_groups() -> Result<HashMap<Subscription, Vec<ResourceGroup>>> {
     let resource_groups =
         gather_from_subscriptions(async |sub: Subscription, _mp: Arc<MultiProgress>| {
             let mut cmd = CommandBuilder::new(CommandKind::AzureCLI);
@@ -20,7 +20,7 @@ pub async fn fetch_all_resource_groups() -> Result<SubscriptionMap<Vec<ResourceG
                 "--output",
                 "json",
                 "--subscription",
-                sub.id.to_string().as_ref(),
+                sub.id.short_form(),
             ]);
             cmd.use_cache_dir(PathBuf::from_iter([
                 "az group list",
