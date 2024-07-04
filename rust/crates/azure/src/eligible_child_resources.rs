48c12aeb571ed4c46278d03dcba550eaf294f5f6
TeamDman <TeamDman9201@gmail.com>
2024-07-04 13:25:23 -0400

    add eligible_child_resources

--- rust/crates/azure/src/eligible_child_resources.rs
+++ rust/crates/azure/src/eligible_child_resources.rs
@@ -3,23 +3,49 @@ use std::time::Duration;
 
 use anyhow::Result;
 use azure_types::prelude::EligibleChildResource;
+use azure_types::prelude::HasScope;
+use azure_types::prelude::Scope;
 use command::prelude::CacheBehaviour;
 use command::prelude::CommandBuilder;
 use command::prelude::CommandKind;
+use fzf::Choice;
+use itertools::Itertools;
 use serde::Deserialize;
+
+use crate::management_groups::fetch_root_management_group;
+use crate::resource_groups::fetch_all_resource_groups;
+
+#[derive(Eq, PartialEq, Debug, Default)]
+pub enum FetchChildrenBehaviour {
+    #[default]
+    Unspecified,
+    GetAllChildren,
+}
 
 // https://learn.microsoft.com/en-us/rest/api/authorization/eligible-child-resources/get?view=rest-authorization-2020-10-01&tabs=HTTP
 pub async fn fetch_eligible_child_resources(
     scope: impl AsRef<str>,
+    behaviour: FetchChildrenBehaviour,
 ) -> Result<Vec<EligibleChildResource>> {
     let scope = scope.as_ref();
-    let url = format!(
+    let scope = scope.strip_prefix('/').unwrap_or(scope);
+    let mut url = format!(
         "https://management.azure.com/{scope}/providers/Microsoft.Authorization/eligibleChildResources?api-version=2020-10-01"
     );
+    if behaviour == FetchChildrenBehaviour::GetAllChildren {
+        url.push_str("&getAllChildren=true");
+    }
     let mut cmd = CommandBuilder::new(CommandKind::AzureCLI);
     cmd.args(["rest", "--method", "GET", "--url", &url]);
+
+    let mut cache_chunks = PathBuf::from("az rest --method GET --url eligibleChildResources");
+    scope
+        .split("/")
+        .filter(|x| !x.is_empty())
+        .for_each(|x| cache_chunks.push(x));
+
     cmd.use_cache_behaviour(CacheBehaviour::Some {
-        path: PathBuf::from_iter(["az rest --method GET --url eligibleChildResources", scope]),
+        path: cache_chunks,
         valid_for: Duration::from_hours(1),
     });
 
@@ -32,26 +58,148 @@ pub async fn fetch_eligible_child_resources(
     Ok(resp.value)
 }
 
+pub async fn fetch_all_eligible_resource_containers() -> Result<Vec<EligibleChildResource>> {
+    let root_mg = fetch_root_management_group().await?;
+    let scope = root_mg.scope().expanded_form();
+    let mut resource_containers =
+        fetch_eligible_child_resources(scope, FetchChildrenBehaviour::GetAllChildren).await?;
+    // this contains management groups and subscriptions
+
+    let rgs = fetch_all_resource_groups()
+        .await?
+        .into_iter()
+        .map(|x| x.1)
+        .flatten()
+        .map(|x| EligibleChildResource {
+            name: x.name.to_owned(),
+            kind: azure_types::prelude::EligibleChildResourceKind::ResourceGroup,
+            id: x.id.expanded_form().to_owned(),
+        });
+    resource_containers.extend(rgs);
+    // extend to include resource groups
+
+    Ok(resource_containers)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::management_groups::fetch_all_management_groups;
+    use crate::management_groups::fetch_root_management_group;
     use azure_types::prelude::HasScope;
     use azure_types::prelude::Scope;
-    use crate::management_groups::fetch_all_management_groups;
-    use super::*;
+    use fzf::pick;
+    use fzf::pick_many;
+    use fzf::Choice;
+    use fzf::FzfArgs;
+    use itertools::Itertools;
 
-    #[tokio::test]
+    #[test_log::test(tokio::test)]
     async fn it_works() -> Result<()> {
-        let mg = fetch_all_management_groups()
-            .await?
-            .into_iter()
-            .find(|mg| mg.display_name == "Tenant Root Group")
-            .unwrap();
+        let mg = fetch_root_management_group().await?;
         let scope = mg.scope().expanded_form();
-        let found = fetch_eligible_child_resources(scope).await?;
+        let found =
+            fetch_eligible_child_resources(scope, FetchChildrenBehaviour::GetAllChildren).await?;
         assert!(found.len() > 0);
         for x in found {
             println!("- {x:?}");
         }
+        Ok(())
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn it_works2() -> Result<()> {
+        let found = fetch_all_eligible_resource_containers().await?;
+        assert!(found.len() > 0);
+        for x in found {
+            println!("- {x:?}");
+        }
+        Ok(())
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn it_works3() -> Result<()> {
+        let rg = fetch_all_resource_groups()
+            .await?
+            .into_iter()
+            .map(|x| x.1)
+            .flatten()
+            .find(|x| x.name == "OPSSc-Dom-Sandbox-RG")
+            .unwrap();
+        let found = fetch_eligible_child_resources(
+            rg.scope().expanded_form(),
+            FetchChildrenBehaviour::GetAllChildren,
+        )
+        .await;
+        assert!(
+            found.is_err(),
+            "GetAllChildren is only supported for management groups?"
+        );
+        Ok(())
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn it_works4() -> Result<()> {
+        let sub = fetch_all_resource_groups()
+            .await?
+            .into_iter()
+            .find(|x| {
+                x.1.iter()
+                    .find(|rg| rg.name == "OPSSc-Dom-Sandbox-RG")
+                    .is_some()
+            })
+            .unwrap()
+            .0;
+        let found = fetch_eligible_child_resources(
+            sub.scope().expanded_form(),
+            FetchChildrenBehaviour::GetAllChildren,
+        )
+        .await;
+        assert!(
+            found.is_err(),
+            "GetAllChildren is only supported for management groups?"
+        );
+        Ok(())
+    }
+
+    #[test_log::test(tokio::test)]
+    #[ignore]
+    async fn it_works_interactive() -> Result<()> {
+        let mg = fetch_root_management_group().await?;
+        let mut scope = mg.scope().expanded_form().to_owned();
+        loop {
+            println!("{}", scope);
+            let next_scope = pick(FzfArgs {
+                choices: fetch_eligible_child_resources(scope, FetchChildrenBehaviour::default())
+                    .await?
+                    .into_iter()
+                    .map(|x| Choice {
+                        display: x.name.to_owned(),
+                        inner: x,
+                    })
+                    .collect_vec(),
+                prompt: None,
+                header: Some("Choose a scope".to_string()),
+            })?;
+            scope = next_scope.inner.id;
+        }
+    }
+    #[test_log::test(tokio::test)]
+    #[ignore]
+    async fn it_works_interactive2() -> Result<()> {
+        let chosen = pick_many(FzfArgs {
+            choices: fetch_all_eligible_resource_containers()
+                .await?
+                .into_iter()
+                .map(|x| Choice {
+                    display: format!("{:?} - {} - {}", x.kind, x.name, x.id),
+                    inner: x,
+                })
+                .collect(),
+            prompt: None,
+            header: None,
+        })?;
+        assert!(chosen.len() > 0);
         Ok(())
     }
 }
--- rust/crates/azure_types/src/eligible_child_resources.rs
+++ rust/crates/azure_types/src/eligible_child_resources.rs
@@ -1,10 +1,129 @@
-use serde::Deserialize;
+use serde::de::Visitor;
+use serde::de::{self};
+use serde::{Deserialize, Serializer};
+use serde::Deserializer;
 use serde::Serialize;
+use std::fmt;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct EligibleChildResource {
     pub name: String,
     #[serde(rename = "type")]
-    pub kind: String,
+    pub kind: EligibleChildResourceKind,
     pub id: String,
+}
+
+
+#[derive(Debug)]
+pub enum EligibleChildResourceKind {
+    ManagementGroup,
+    Subscription,
+    ResourceGroup,
+    Other(String),
+}
+
+impl Serialize for EligibleChildResourceKind {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            EligibleChildResourceKind::ManagementGroup => serializer.serialize_str("managementgroup"),
+            EligibleChildResourceKind::Subscription => serializer.serialize_str("subscription"),
+            EligibleChildResourceKind::ResourceGroup => serializer.serialize_str("resourcegroup"),
+            EligibleChildResourceKind::Other(ref s) => serializer.serialize_str(s),
+        }
+    }
+}
+
+struct EligibleChildResourceKindVisitor;
+
+impl<'de> Visitor<'de> for EligibleChildResourceKindVisitor {
+    type Value = EligibleChildResourceKind;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a string representing the resource kind")
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<EligibleChildResourceKind, E>
+    where
+        E: de::Error,
+    {
+        Ok(match value {
+            "managementgroup" => EligibleChildResourceKind::ManagementGroup,
+            "subscription" => EligibleChildResourceKind::Subscription,
+            "resourcegroup" => EligibleChildResourceKind::ResourceGroup,
+            other => EligibleChildResourceKind::Other(other.to_string()),
+        })
+    }
+}
+
+impl<'de> Deserialize<'de> for EligibleChildResourceKind {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(EligibleChildResourceKindVisitor)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn test_serialization() {
+        let resource = EligibleChildResource {
+            name: String::from("Example Resource"),
+            kind: EligibleChildResourceKind::ManagementGroup,
+            id: String::from("resource-id-123"),
+        };
+
+        let json = serde_json::to_string(&resource).unwrap();
+        let expected_json = r#"{"name":"Example Resource","type":"managementgroup","id":"resource-id-123"}"#;
+
+        assert_eq!(json, expected_json);
+    }
+
+    #[test]
+    fn test_deserialization() {
+        let json = r#"{"name":"Example Resource","type":"managementgroup","id":"resource-id-123"}"#;
+        let resource: EligibleChildResource = serde_json::from_str(json).unwrap();
+
+        assert_eq!(resource.name, "Example Resource");
+        assert_eq!(resource.id, "resource-id-123");
+        match resource.kind {
+            EligibleChildResourceKind::ManagementGroup => {}
+            _ => panic!("Expected ManagementGroup"),
+        }
+    }
+
+    #[test]
+    fn test_serialization_with_other() {
+        let resource = EligibleChildResource {
+            name: String::from("Another Resource"),
+            kind: EligibleChildResourceKind::Other(String::from("customtype")),
+            id: String::from("resource-id-456"),
+        };
+
+        let json = serde_json::to_string(&resource).unwrap();
+        let expected_json = r#"{"name":"Another Resource","type":"customtype","id":"resource-id-456"}"#;
+
+        assert_eq!(json, expected_json);
+    }
+
+    #[test]
+    fn test_deserialization_with_other() {
+        let json = r#"{"name":"Another Resource","type":"customtype","id":"resource-id-456"}"#;
+        let resource: EligibleChildResource = serde_json::from_str(json).unwrap();
+
+        assert_eq!(resource.name, "Another Resource");
+        assert_eq!(resource.id, "resource-id-456");
+        match resource.kind {
+            EligibleChildResourceKind::Other(ref s) if s == "customtype" => {}
+            _ => panic!("Expected Other with value 'customtype'"),
+        }
+    }
 }
