1c04fbc46dfe84ad4eb17a8b8109b4da40370b8c
TeamDman <TeamDman9201@gmail.com>
2024-07-15 14:55:43 -0400

    save default config to disk

--- rust/crates/config/src/lib.rs
+++ rust/crates/config/src/lib.rs
@@ -1,40 +1,19 @@
-#![feature(try_blocks)]
-use std::fs::OpenOptions;
-use std::io::Read;
-
+use anyhow::bail;
 use anyhow::Context;
 use anyhow::Result;
 use once_cell::sync::Lazy;
 use pathing::AppDir;
 use serde::Deserialize;
 use serde::Serialize;
+use std::fs::OpenOptions;
+use std::io::Read;
+use std::io::Write;
+use std::path::PathBuf;
 use tracing::debug;
+use tracing::error;
 use tracing::warn;
 
-static CONFIG: Lazy<Config> = Lazy::new(|| {
-    let from_disk: Result<Config> = try {
-        let config_path = AppDir::Config.as_path_buf().join("config.json");
-        let mut file = OpenOptions::new()
-            .read(true)
-            .open(&config_path)
-            .context(format!("opening config file {}", config_path.display()))?;
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).context(format!(
-            "reading file contents from {}",
-            config_path.display()
-        ))?;	
-        let config = serde_json::from_str(&contents).context("parsing contents as config")?;
-        debug!("Successfully loaded config from {}", config_path.display());
-        config
-    };
-    match from_disk {
-        Ok(config) => config,
-        Err(e) => {
-            warn!("Failed to load config, using default. Error: {:?}", e);
-            Config::default()
-        }
-    }
-});
+static CONFIG: Lazy<Config> = Lazy::new(|| get_or_create_config());
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Config {
@@ -80,4 +59,65 @@ impl Default for Config {
             },
         }
     }
+}
+
+fn get_or_create_config() -> Config {
+    let config_dir = AppDir::Config.as_path_buf();
+    let config_path = config_dir.join("config.json");
+    match load_config_from_disk(&config_path) {
+        Ok(config) => config,
+        Err(e) => {
+            warn!(
+                "Failed to load config, using default and writing it to disk. Error: {:?}",
+                e
+            );
+            let config = Config::default();
+            if let Err(e) = write_config_to_disk(&config, &config_path) {
+                error!("Failed to write default config to disk! {:?}", e);
+            }
+
+            config
+        }
+    }
+}
+
+fn load_config_from_disk(config_path: &PathBuf) -> Result<Config> {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .open(&config_path)
+        .context(format!(
+            "opening config file for reading \"{}\"",
+            config_path.display()
+        ))?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).context(format!(
+        "reading file contents from {}",
+        config_path.display()
+    ))?;
+    let config = serde_json::from_str::<Config>(&contents).context("parsing contents as config")?;
+    debug!("Successfully loaded config from {}", config_path.display());
+    Ok(config)
+}
+
+fn write_config_to_disk(config: &Config, config_path: &PathBuf) -> Result<()> {
+    let Some(config_dir) = config_path.parent() else {
+        bail!("Config path doesn't have a parent dir? path={}", config_path.display());
+    };
+    std::fs::create_dir_all(&config_dir).context(format!(
+        "ensuring config dir \"{}\" exists",
+        config_dir.display()
+    ))?;
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(&config_path)
+        .context(format!(
+            "opening config file for writing \"{}\"",
+            config_path.display()
+        ))?;
+    let content = serde_json::to_string_pretty(&config).context("serializing config")?;
+    file.write_all(content.as_bytes())
+        .context(format!("writing bytes to \"{}\"", config_path.display()))?;
+    Ok(())
 }
