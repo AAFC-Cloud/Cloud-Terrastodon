17aaedab20ea2461ddd4ecb35e2b66c421fea1c5
TeamDman <TeamDman9201@gmail.com>
2024-08-13 10:23:25 -0400

    add sub and managementgroup to name helper

--- rust/crates/azure/src/name_lookup_helper.rs
+++ rust/crates/azure/src/name_lookup_helper.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use anyhow::Result;
 use azure_types::prelude::Scope;
 use azure_types::prelude::ScopeImpl;
@@ -5,11 +6,14 @@ use azure_types::prelude::ScopeImplKind;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use tracing::info;
+use tracing::warn;
 
+use crate::prelude::fetch_all_management_groups;
 use crate::prelude::fetch_all_policy_assignments;
 use crate::prelude::fetch_all_policy_definitions;
 use crate::prelude::fetch_all_policy_set_definitions;
 use crate::prelude::fetch_all_resource_groups;
+use crate::prelude::fetch_all_subscriptions;
 
 #[derive(Default)]
 pub struct NameLookupHelper {
@@ -19,7 +23,9 @@ impl NameLookupHelper {
     pub async fn get_name_for_scope(&mut self, scope: &ScopeImpl) -> Result<Option<&String>> {
         let kind = scope.kind();
         if let Entry::Vacant(e) = self.cache.entry(kind) {
-            let names = fetch_names_for(kind).await?;
+            let names = fetch_names_for(kind).await.context(format!(
+                "Error after cache miss getting name for scope {scope}"
+            ))?;
             e.insert(names);
         }
         Ok(self.cache.get(&kind).and_then(|names| names.get(scope)))
@@ -33,25 +39,43 @@ pub async fn fetch_names_for(kind: ScopeImplKind) -> Result<HashMap<ScopeImpl, S
             .await?
             .into_values()
             .flatten()
-            .map(|v| (v.id.as_scope(), v.name))
+            .map(|x| (x.id.as_scope(), x.name))
             .collect(),
         ScopeImplKind::PolicySetDefinition => fetch_all_policy_set_definitions()
             .await?
             .into_values()
             .flatten()
-            .map(|v| (v.id.as_scope(), v.name))
+            .map(|x| (x.id.as_scope(), x.name))
             .collect(),
         ScopeImplKind::PolicyAssignment => fetch_all_policy_assignments()
             .await?
             .into_values()
             .flatten()
-            .map(|v| (v.id.as_scope(), v.name))
+            .map(|x| (x.id.as_scope(), x.name))
             .collect(),
         ScopeImplKind::ResourceGroup => fetch_all_resource_groups()
             .await?
             .into_iter()
-            .map(|rg| (rg.id.as_scope(), rg.name))
+            .map(|x| (x.id.as_scope(), x.name))
             .collect(),
-        x => todo!("Name lookup for data block generation missing impl for {x:?}"),
+        ScopeImplKind::ManagementGroup => fetch_all_management_groups()
+            .await?
+            .into_iter()
+            .map(|x| (x.id.as_scope(), x.name))
+            .collect(),
+        ScopeImplKind::Subscription => fetch_all_subscriptions()
+            .await?
+            .into_iter()
+            .map(|x| (x.id.as_scope(), x.name))
+            .collect(),
+        _ => {
+            warn!(
+                "Name lookup for data block generation missing impl for {kind:?} in {} {}:{}",
+                file!(),
+                line!(),
+                column!()
+            );
+            Default::default()
+        }
     })
 }
