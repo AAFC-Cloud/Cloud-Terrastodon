6d10368c70c51dba489212f0c6e6517cd9e09f72
TeamDman <TeamDman9201@gmail.com>
2024-08-23 14:54:56 -0400

    add toggle for physics debug

--- visualizer/src/main.rs
+++ visualizer/src/main.rs
@@ -1,5 +1,6 @@
 use avian2d::prelude::Gravity;
 use avian2d::prelude::PhysicsDebugPlugin;
+use avian2d::prelude::PhysicsGizmos;
 use avian2d::PhysicsPlugins;
 use bevy::input::common_conditions::input_toggle_active;
 use bevy::log::LogPlugin;
@@ -39,7 +40,23 @@ fn main() {
     app.add_plugins(AzurePlugin);
     app.add_plugins(CursorPlugin);
     app.add_plugins(DampingPlugin);
-    app.add_plugins(PhysicsDebugPlugin::default());
+    #[cfg(debug_assertions)]
+    {
+        app.add_plugins(PhysicsDebugPlugin::default());
+        {
+            let mut store = app.world_mut().resource_mut::<GizmoConfigStore>();
+            let config = store.config_mut::<PhysicsGizmos>().0;
+            config.enabled = false;
+        }
+        app.add_systems(
+            Update,
+            |inputs: Res<ButtonInput<KeyCode>>, mut store: ResMut<GizmoConfigStore>| {
+                store.config_mut::<PhysicsGizmos>().0.enabled ^=
+                    inputs.just_pressed(KeyCode::Backquote);
+            },
+        );
+    }
+
     app.add_plugins(PhysicsPlugins::default().with_length_unit(100.0));
     app.insert_resource(Gravity(Vec2::ZERO));
 
