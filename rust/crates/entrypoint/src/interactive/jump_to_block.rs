08c9ad8ebdbf2f6714af8a844e80b19c91c75e98
TeamDman <TeamDman9201@gmail.com>
2024-08-30 15:51:34 -0400

    remove duplicate code by using new list_blocks_for_dir helper

--- rust/crates/entrypoint/src/interactive/jump_to_block.rs
+++ rust/crates/entrypoint/src/interactive/jump_to_block.rs
@@ -5,38 +5,14 @@ use cloud_terrastodon_core_command::prelude::CommandKind;
 use cloud_terrastodon_core_command::prelude::OutputBehaviour;
 use cloud_terrastodon_core_fzf::pick;
 use cloud_terrastodon_core_fzf::FzfArgs;
-use cloud_terrastodon_core_tofu::prelude::list_blocks_for_file;
+use cloud_terrastodon_core_tofu::prelude::list_blocks_for_dir;
 use std::path::PathBuf;
-use tokio::fs;
 use tracing::info;
 
 pub async fn jump_to_block(dir: PathBuf) -> Result<()> {
     // We don't use `as_single_body` because we need to track the files that each block comes from
-    let mut files = fs::read_dir(dir).await.context("reading files")?;
-    let mut choices = Vec::new();
-    let mut num_files = 0;
-    while let Some(tf_file) = files.next_entry().await.context("reading entry")? {
-        let kind = tf_file.file_type().await.context("getting file type")?;
-        if !kind.is_file() {
-            continue;
-        }
-        let path = tf_file.path();
-        if path
-            .extension()
-            .filter(|ext| ext.to_string_lossy() == "tf")
-            .is_none()
-        {
-            continue;
-        }
-
-        num_files += 1;
-        info!("Gathering blocks from {}", path.display());
-
-        let mut blocks = list_blocks_for_file(path).await.context("listing blocks")?;
-        choices.append(&mut blocks);
-    }
-
-    info!("Found {} blocks across {} files", choices.len(), num_files);
+    let choices = list_blocks_for_dir(dir).await?;
+    info!("Found {} blocks", choices.len());
 
     let chosen = pick(FzfArgs {
         choices,
