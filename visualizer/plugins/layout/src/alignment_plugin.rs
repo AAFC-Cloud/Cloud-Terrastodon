05154b9f588d6da66ca507811678a405a0e7ffe8
TeamDman <TeamDman9201@gmail.com>
2024-10-17 15:47:25 -0400

    Alignment working kinda, improvements to be made
    could maybe:
    - also apply force to parent when regions overlap
    - also apply force to parent when children span not containing parent

--- visualizer/plugins/layout/src/alignment_plugin.rs
+++ visualizer/plugins/layout/src/alignment_plugin.rs
@@ -10,6 +10,9 @@ use bevy::utils::HashSet;
 use indexmap::IndexMap;
 use indexmap::IndexSet;
 use itertools::Itertools;
+use std::cmp::max;
+use std::cmp::min;
+use std::ops::RangeInclusive;
 
 pub struct AlignmentPlugin;
 
@@ -17,7 +20,7 @@ impl Plugin for AlignmentPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, left_to_right_flow);
         app.add_systems(Update, vertical_alignment);
-        app.add_systems(Update, vertical_separation);
+        app.add_systems(Update, vertical_placement);
     }
 }
 
@@ -57,6 +60,26 @@ fn get_neighbourhoods(
     neighbourhoods
 }
 
+fn sort_neighbourhoods(
+    neighbourhoods: &HashMap<Entity, HashSet<Entity>>,
+    y_values: &HashMap<Entity, f32>,
+) -> IndexMap<Entity, IndexSet<Entity>> {
+    neighbourhoods
+        .into_iter()
+        .sorted_by_cached_key(|(key, _)| (y_values.get(*key).unwrap_or(&0.0) * 1024.0) as i32)
+        .map(|(key, value)| {
+            let value = value
+                .iter()
+                .sorted_by_cached_key(|e| (y_values.get(*e).unwrap_or(&0.0) * 1024.0) as i32)
+                .rev()
+                .cloned()
+                .collect::<IndexSet<Entity>>();
+            (*key, value)
+        })
+        .rev()
+        .collect()
+}
+
 fn vertical_alignment(
     mut lf_joint_query: Query<&DistanceJoint, With<LeaderFollowerJoint>>,
     mut phys_query: Query<(&Position, &mut LinearVelocity)>,
@@ -84,7 +107,18 @@ fn vertical_alignment(
     }
 }
 
-fn vertical_separation(
+fn overlap_size(range1: &RangeInclusive<i32>, range2: &RangeInclusive<i32>) -> i32 {
+    let start = max(*range1.start(), *range2.start());
+    let end = min(*range1.end(), *range2.end());
+
+    if start <= end {
+        end - start + 1 // +1 because RangeInclusive includes the end value
+    } else {
+        0 // No overlap
+    }
+}
+
+fn vertical_placement(
     mut lf_joint_query: Query<&DistanceJoint, With<LeaderFollowerJoint>>,
     mut phys_query: Query<(Entity, &Position, &mut LinearVelocity)>,
 ) {
@@ -94,36 +128,58 @@ fn vertical_separation(
     }
 
     let neighbourhoods = get_neighbourhoods(lf_joint_query.as_query_lens());
-    let neighbourhoods = neighbourhoods
-        .into_iter()
-        .sorted_by_cached_key(|(key, _)| (y_values.get(key).unwrap_or(&0.0) * 1000.0) as i32)
-        .map(|(key, value)| {
-            let value = value
-                .into_iter()
-                .sorted_by_cached_key(|e| (y_values.get(e).unwrap_or(&0.0) * 1000.0) as i32)
-                .rev()
-                .collect::<IndexSet<Entity>>();
-            (key, value)
-        })
-        .rev()
-        .collect::<IndexMap<Entity, IndexSet<Entity>>>();
-
-    for ((_, above_followers), (below_leader, _)) in neighbourhoods.iter().tuple_windows() {
-        // the above followers may be above the below leader
-        // if so, we want to apply an upwards force to the above followers
-        let below_leader_y = y_values.get(below_leader).cloned().unwrap_or_default();
-        let last_above_follower_y = above_followers
-            .last()
-            .and_then(|e| y_values.get(e))
-            .cloned()
-            .unwrap_or_default();
-        let diff = below_leader_y - last_above_follower_y;
-        if diff > 0.0 {
-            for above_follower in above_followers {
-                let Ok((_, _, mut vel)) = phys_query.get_mut(*above_follower) else {
-                    continue;
+    let neighbourhoods = sort_neighbourhoods(&neighbourhoods, &y_values);
+    for children in neighbourhoods.values() {
+        // the vertical span of each child node should not overlap with the vertical span of any other child
+        let children_vertical_spans = children
+            .iter()
+            .filter_map(|child| {
+                // get the range of y values of the children of the child
+                let Some(child_children) = neighbourhoods.get(child) else {
+                    return None;
                 };
-                vel.y += diff;
+                let y_range = child_children
+                    .iter()
+                    .map(|child| y_values.get(child).unwrap())
+                    .fold(i32::MAX..=i32::MIN, |acc, &y| {
+                        let start: i32 = *acc.start().min(&(y as i32));
+                        let end: i32 = *acc.end().max(&(y as i32));
+                        start..=end
+                    });
+                Some((*child, y_range, child_children))
+            })
+            .collect_vec();
+        for (parent, span, children) in children_vertical_spans.iter() {
+            // the span of children should be centered on the parent
+            let span_center = span.start() + (span.end() - span.start()) / 2;
+            let diff = span_center as f32 - y_values.get(parent).unwrap();
+            if diff.abs() > 2.0 {
+                // apply a force to all children to move the span
+                for child in children.iter() {
+                    if let Ok((_, _, mut vel)) = phys_query.get_mut(*child) {
+                        vel.y += -diff as f32 * 0.05;
+                    }
+                }
+            }
+        }
+        for ((_, above_span, above_children), (_, below_span, below_children)) in
+            children_vertical_spans.iter().tuple_windows()
+        {
+            // apply force to overlapping regions
+            let overlap = overlap_size(&above_span, &below_span);
+            if overlap > 0 {
+                // a force should be applied such that the children spread apart
+                // the bottom-most above-child should receive an upwards force
+                // the top-most below-child should receive a downwards force
+                let bottom_most_above_child = above_children.last().unwrap();
+                let top_most_below_child = below_children.first().unwrap();
+                if let Ok((_, _, mut vel)) = phys_query.get_mut(*bottom_most_above_child) {
+                    vel.y += overlap as f32 * 20.0;
+                }
+                if let Ok((_, _, mut vel)) = phys_query.get_mut(*top_most_below_child) {
+                    vel.y -= overlap as f32 * 20.0;
+                }
+                debug!("Found overlap!");
             }
         }
     }
