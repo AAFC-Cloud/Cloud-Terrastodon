a3d4755c96843fc73b8c9bfe046a9b827558796a
TeamDman <TeamDman9201@gmail.com>
2024-10-29 16:05:07 -0400

    id reference patcher to work with role assignments

--- rust/crates/tofu/src/user_id_reference_patcher.rs
+++ rust/crates/tofu/src/user_id_reference_patcher.rs
@@ -2,16 +2,17 @@ use anyhow::Result;
 use cloud_terrastodon_core_azure::prelude::uuid::Uuid;
 use cloud_terrastodon_core_tofu_types::prelude::AsTofuString;
 use cloud_terrastodon_core_tofu_types::prelude::TofuAzureADResourceKind;
+use cloud_terrastodon_core_tofu_types::prelude::TofuAzureRMResourceKind;
 use cloud_terrastodon_core_tofu_types::prelude::TofuDataBlock;
 use cloud_terrastodon_core_tofu_types::prelude::TofuResourceKind;
 use cloud_terrastodon_core_tofu_types::prelude::TryAsTofuBlocks;
+use hcl::edit::expr::Array;
 use hcl::edit::expr::Expression;
-use hcl::edit::structure::AttributeMut;
 use hcl::edit::structure::Body;
-use hcl::edit::visit_mut::visit_attr_mut;
 use hcl::edit::visit_mut::visit_block_mut;
 use hcl::edit::visit_mut::VisitMut;
 use hcl::edit::Decorate;
+use tracing::debug;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
@@ -57,51 +58,73 @@ impl UserIdReferencePatcher {
 
         Ok(Some(body))
     }
+
+    fn convert_array(&mut self, array: &mut Array) {
+        for entry in array.iter_mut() {
+            if self.convert_expr_if_user_id(entry) {
+                let decor = entry.decor_mut();
+                decor.set_prefix("\n");
+            }
+        }
+        array.set_trailing_comma(true);
+        array.set_trailing("\n");
+    }
+
+    fn convert_expr_if_user_id(&mut self, expr: &mut Expression) -> bool {
+        if let Some(value) = expr.as_str()
+            && let Ok(id) = Uuid::parse_str(value)
+            && let Some(mail) = self.user_principal_name_by_user_id.get(&id)
+            && let Ok(new_expr) = format!("local.users[\"{}\"]", mail).parse::<Expression>()
+        {
+            *expr = new_expr;
+            self.used.insert(id);
+            true
+        } else {
+            false
+        }
+    }
 }
 impl VisitMut for UserIdReferencePatcher {
     fn visit_block_mut(&mut self, node: &mut hcl::edit::structure::Block) {
-        visit_block_mut(self, node);
-        // Comment out empty owners/members attributes to satisfy terraform validate
-        if node
+        let tofu_resource_kind = node
             .labels
             .first()
             .map(|x| x.as_str())
-            .and_then(|x| x.parse::<TofuResourceKind>().ok())
-            == Some(TofuResourceKind::AzureAD(TofuAzureADResourceKind::Group))
-        {
-            for key in ["owners", "members"] {
-                if let Some(ref mut attr) = node.body.get_attribute_mut(key)
-                    && let Some(ref mut array) = attr.value_mut().as_array_mut()
-                    && array.is_empty()
-                {
-                    array.set_trailing("");
-                    attr.decor_mut().set_prefix("#");
+            .and_then(|x| x.parse::<TofuResourceKind>().ok());
+
+        // Comment out empty owners/members attributes to satisfy terraform validate
+        match tofu_resource_kind {
+            Some(TofuResourceKind::AzureAD(TofuAzureADResourceKind::Group)) => {
+                debug!("Converting owners and members for azuread_security_group");
+                for key in ["owners", "members"] {
+                    if let Some(ref mut attr) = node.body.get_attribute_mut(key)
+                        && let Some(ref mut array) = attr.value_mut().as_array_mut()
+                    {
+                        if array.is_empty() {
+                            array.set_trailing("");
+                            attr.decor_mut().set_prefix("#");
+                        } else {
+                            self.convert_array(array);
+                        }
+                    }
                 }
             }
+            Some(TofuResourceKind::AzureRM(TofuAzureRMResourceKind::RoleAssignment)) => {
+                debug!("Converting principal_id for azurerm_role_assignment");
+                if node
+                    .body
+                    .get_attribute("principal_type")
+                    .and_then(|x| x.value.as_str())
+                    == Some("User")
+                {
+                    if let Some(mut attr) = node.body.get_attribute_mut("principal_id") {
+                        self.convert_expr_if_user_id(attr.value_mut());
+                    }
+                }
+            }
+            _ => {}
         }
-    }
-    fn visit_attr_mut(&mut self, mut node: AttributeMut) {
-        let key = node.key.to_string();
-        let key = key.trim();
-        if (key == "owners" || key == "members")
-            && let Some(array) = node.value_mut().as_array_mut()
-        {
-            for entry in array.iter_mut() {
-                if let Some(value) = entry.as_str()
-                    && let Ok(id) = Uuid::parse_str(value)
-                    && let Some(mail) = self.user_principal_name_by_user_id.get(&id)
-                    && let Ok(expr) = format!("local.users[\"{}\"]", mail).parse::<Expression>()
-                {
-                    *entry = expr;
-                    let decor = entry.decor_mut();
-                    decor.set_prefix("\n");
-                    self.used.insert(id);
-                }
-            }
-            array.set_trailing_comma(true);
-            array.set_trailing("\n");
-        } else {
-            visit_attr_mut(self, node);
-        };
+
+        visit_block_mut(self, node);
     }
 }
