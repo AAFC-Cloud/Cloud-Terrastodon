88b6668ca59f1218a099c41665834c2123ab2f0d
TeamDman <TeamDman9201@gmail.com>
2025-01-10 10:11:20 -0500

    Fetch policy info

--- rust/crates/entrypoint/src/interactive/search_assigned_policies.rs
+++ rust/crates/entrypoint/src/interactive/search_assigned_policies.rs
@@ -1,4 +1,27 @@
+use anyhow::Context;
+use cloud_terrastodon_core_azure::prelude::fetch_all_policy_assignments;
+use cloud_terrastodon_core_azure::prelude::fetch_all_policy_definitions;
+use cloud_terrastodon_core_azure::prelude::fetch_all_policy_set_definitions;
+use tokio::try_join;
+use tracing::info;
+
 pub async fn search_assigned_policies() -> anyhow::Result<()> {
-    println!("hi!");
+    info!("Fetching policy assignments and definitions");
+    let (policy_assignments, policy_definitions, policy_set_definitions) = match try_join!(
+        fetch_all_policy_assignments(),
+        fetch_all_policy_definitions(),
+        fetch_all_policy_set_definitions()
+    ) {
+        Ok(x) => x,
+        Err(e) => return Err(e).context("failed to fetch policy data"),
+    };
+
+    info!("Found {} policy assignments", policy_assignments.len());
+    info!("Found {} policy definitions", policy_definitions.len());
+    info!(
+        "Found {} policy set definitions",
+        policy_set_definitions.len()
+    );
+
     Ok(())
 }
