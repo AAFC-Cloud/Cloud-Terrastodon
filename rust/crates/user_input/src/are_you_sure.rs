6a6912d9a84dcdbf6282a5a47504a46d1924dbb0
TeamDman <TeamDman9201@gmail.com>
2025-02-14 11:22:32 -0500

    Ensure "no" is default for are_you_sure prompt

--- rust/crates/user_input/src/are_you_sure.rs
+++ rust/crates/user_input/src/are_you_sure.rs
@@ -1,11 +1,11 @@
 use crate::prelude::pick;
 use crate::prelude::FzfArgs;
 
-pub fn are_you_sure(message: String) -> eyre::Result<bool> {
-    let choices = ["Yes", "No"];
+pub fn are_you_sure(message: impl AsRef<str>) -> eyre::Result<bool> {
+    let choices = ["No", "Yes"];
     let chosen = pick(FzfArgs {
         choices: choices.into(),
-        header: Some(message),
+        header: Some(message.as_ref().to_string()),
         prompt: Some("Are you sure? ".to_string()),
     })?;
     Ok(chosen == "Yes")
