a28207e6e70cf2d3a3ed092dfcc8d56f606d13af
TeamDman <TeamDman9201@gmail.com>
2025-03-11 15:15:53 -0400

    Add warning when system is detected to not have UTF-8 enabled

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -31,6 +31,14 @@ mod windows_ansi {
     }
 }
 
+#[cfg(windows)]
+mod windows_utf8 {
+    use windows::Win32::Globalization::GetACP;
+    pub fn is_system_utf8() -> bool {
+        unsafe { GetACP() == 65001 }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
@@ -64,6 +72,13 @@ async fn main() -> Result<()> {
     // show no errors when colours unavailable (piping situations)
     #[cfg(windows)]
     let _ = windows_ansi::enable_ansi_support();
+
+    #[cfg(windows)]
+    if !windows_utf8::is_system_utf8() {
+        tracing::warn!("The current system codepage is not UTF-8. This may cause '�' problems.");
+        tracing::warn!("See https://github.com/Azure/azure-cli/issues/22616#issuecomment-1147061949");
+        tracing::warn!("Control panel -> Clock and Region -> Region -> Administrative -> Change system locale -> Check Beta: Use Unicode UTF-8 for worldwide language support.");
+    }
 
     // go to menu
     entrypoint_main(Version::new(env!("CARGO_PKG_VERSION").to_string())).await?;
