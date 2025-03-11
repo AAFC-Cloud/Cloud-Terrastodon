f38b53c125e7700a5cb7631e89b86fdfd50a5322
TeamDman <TeamDman9201@gmail.com>
2025-03-11 16:01:44 -0400

    add batch repo fetching

--- rust/crates/azure_devops/src/azure_devops_repos.rs
+++ rust/crates/azure_devops/src/azure_devops_repos.rs
@@ -3,9 +3,11 @@ use cloud_terrastodon_core_azure_devops_types::prelude::AzureDevOpsRepo;
 use cloud_terrastodon_core_command::prelude::CacheBehaviour;
 use cloud_terrastodon_core_command::prelude::CommandBuilder;
 use cloud_terrastodon_core_command::prelude::CommandKind;
+use eyre::Context;
 use eyre::Result;
 use std::path::PathBuf;
 use std::time::Duration;
+use tokio::task::JoinSet;
 use tracing::info;
 
 pub async fn fetch_all_azure_devops_repos_for_project(
@@ -22,12 +24,36 @@ pub async fn fetch_all_azure_devops_repos_for_project(
         project_id.to_string().as_ref(),
     ]);
     cmd.use_cache_behaviour(CacheBehaviour::Some {
-        path: PathBuf::from_iter(["az", "repos", "list", project_id.to_string().replace(" ","_").as_ref()]),
+        path: PathBuf::from_iter([
+            "az",
+            "repos",
+            "list",
+            project_id.to_string().replace(" ", "_").as_ref(),
+        ]),
         valid_for: Duration::from_hours(8),
     });
     let repos: Vec<AzureDevOpsRepo> = cmd.run().await?;
-    info!("Found {} repos", repos.len());
+    info!("Found {} repos for {project_id:?}", repos.len());
     Ok(repos)
+}
+
+pub async fn fetch_azure_devops_repos_batch(
+    project_ids: Vec<AzureDevOpsProjectId>,
+) -> Result<Vec<(AzureDevOpsProjectId, Vec<AzureDevOpsRepo>)>> {
+    let mut rtn = Vec::new();
+    let mut set = JoinSet::new();
+    for project_id in project_ids {
+        set.spawn(async move {
+            let repos = fetch_all_azure_devops_repos_for_project(&project_id).await;
+            (project_id, repos)
+        });
+    }
+    while let Some(res) = set.join_next().await {
+        let (project_id, repos) = res?;
+        let repos = repos.wrap_err(format!("Fetching repos for project {project_id:?}"))?;
+        rtn.push((project_id, repos));
+    }
+    Ok(rtn)
 }
 
 #[cfg(test)]
