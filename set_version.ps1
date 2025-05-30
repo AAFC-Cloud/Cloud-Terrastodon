f02d0c5590ced6f4e1283626367b83338d5cbfab
TeamDman <TeamDman9201@gmail.com>
2025-05-30 12:06:41 -0400

    fix set_version script

--- set_version.ps1
+++ set_version.ps1
@@ -38,7 +38,9 @@ $cargoTomlPath = Join-Path $PSScriptRoot "Cargo.toml"
 $cargoTomlContent = Get-Content $cargoTomlPath -Raw
 
 # Update root Cargo.toml version fields
-$updatedCargoTomlContent = $cargoTomlContent -replace '"\d+\.\d+\.\d+"(\s*#\s*CT_VERSION)', '"' + $newVersion + '$1'
+$patternRoot = '"\d+\.\d+\.\d+"(\s*#\s*CT_VERSION)'
+$replacementRoot = '"' + $newVersion + '"$1'
+$updatedCargoTomlContent = $cargoTomlContent -replace $patternRoot, $replacementRoot
 Set-Content -Path $cargoTomlPath -Value $updatedCargoTomlContent
 Write-Host "Updated root Cargo.toml to version $newVersion"
 
@@ -48,7 +50,9 @@ Get-ChildItem -Path $cratesDir -Directory | ForEach-Object {
     $crateTomlPath = Join-Path $_.FullName "Cargo.toml"
     if (Test-Path $crateTomlPath) {
         $crateContent = Get-Content $crateTomlPath -Raw
-        $updatedCrateContent = $crateContent -replace '(?m)^(version\s*=\s*)"\d+\.\d+\.\d+"', '$1"' + $newVersion + '"'
+        $patternCrate = '(?m)^(version\s*=\s*)"\d+\.\d+\.\d+"'
+        $replacementCrate = '$1"' + $newVersion + '"'
+        $updatedCrateContent = $crateContent -replace $patternCrate, $replacementCrate
         Set-Content -Path $crateTomlPath -Value $updatedCrateContent
         Write-Host "Updated $crateTomlPath to version $newVersion"
     }
