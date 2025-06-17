dc362d16d1442d4b127ac995a026bf8b4046b595
TeamDman <TeamDman9201@gmail.com>
2025-06-17 08:49:02 -0400

    add comment on serde helper to show where came from

--- crates/azure_types/src/serde_helpers.rs
+++ crates/azure_types/src/serde_helpers.rs
@@ -1,6 +1,7 @@
 use serde::Deserialize;
 use serde::Deserializer;
 
+/// https://github.com/serde-rs/serde/issues/1098 - Ability to use default value even if set to null
 pub fn deserialize_null_default<'de, D, T>(deserializer: D) -> Result<T, D::Error>
 where
     T: Default + Deserialize<'de>,
