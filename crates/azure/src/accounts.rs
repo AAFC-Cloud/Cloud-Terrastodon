c95c00627b66e2760a7e2c07ac6041ddc2a14223
TeamDman <TeamDman9201@gmail.com>
2025-07-04 09:51:36 -0400

    add account test raw

--- crates/azure/src/accounts.rs
+++ crates/azure/src/accounts.rs
@@ -18,6 +18,10 @@ pub async fn az_account_list() -> eyre::Result<Vec<Account>> {
 
 #[cfg(test)]
 mod test {
+    use std::{path::PathBuf, time::Duration};
+
+    use cloud_terrastodon_command::{CacheBehaviour, CommandBuilder, CommandKind};
+
     use crate::prelude::az_account_list;
 
     #[tokio::test]
@@ -25,6 +29,21 @@ mod test {
         let accounts = az_account_list().await?;
         dbg!(&accounts);
         assert_ne!(accounts.as_slice(), []);
+        Ok(())
+    }
+
+    #[tokio::test]
+    pub async fn it_works_raw() -> eyre::Result<()> {
+            
+        let mut cmd = CommandBuilder::new(CommandKind::AzureCLI);
+        cmd.args(["account", "list", "--output", "json"]);
+        cmd.use_cache_behaviour(CacheBehaviour::Some {
+            valid_for: Duration::from_secs(30),
+            path: PathBuf::from_iter(["az", "account", "list"]),
+        });
+        let rtn = cmd.run_raw().await?;
+        dbg!(&rtn);
+        assert!(rtn.status == 0);
         Ok(())
     }
 }
