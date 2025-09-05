bbd336733a3b9f90e0d056f6d4ac1f63eef97131
TeamDman <TeamDman9201@gmail.com>
2025-09-05 13:57:00 -0400

    fix pim role fetching

--- crates/azure/src/governance_role_assignments.rs
+++ crates/azure/src/governance_role_assignments.rs
@@ -5,12 +5,13 @@ use cloud_terrastodon_command::CacheBehaviour;
 use std::path::PathBuf;
 use std::time::Duration;
 
+/// See also: https://github.com/Azure/azure-cli/issues/28854
 pub async fn fetch_governance_role_assignments_for_principal(
     principal_id: impl Into<PrincipalId>,
 ) -> eyre::Result<Vec<GovernanceRoleAssignment>> {
     let principal_id: PrincipalId = principal_id.into();
     let url = format!(
-        "https://graph.microsoft.com/beta/privilegedAccess/aadroles/roleAssignments?$expand=linkedEligibleRoleAssignment,scopedResource,subject,roleDefinition($expand=resource)&$filter=(subject/id eq '{}')",
+        "https://graph.microsoft.com/beta/privilegedAccess/aadroles/roleAssignments?$expand=linkedEligibleRoleAssignment,subject,roleDefinition($expand=resource)&$filter=(subject/id eq '{}')",
         principal_id
     );
     MicrosoftGraphHelper::new(
