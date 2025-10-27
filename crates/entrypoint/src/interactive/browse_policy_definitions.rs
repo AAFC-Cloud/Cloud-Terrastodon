74e3b31f54a5919c590cd4318d2ed04e0a7478cf
TeamDman <TeamDman9201@gmail.com>
2025-10-27 13:55:23 -0400

    update policy browser to show policy description

--- crates/entrypoint/src/interactive/browse_policy_definitions.rs
+++ crates/entrypoint/src/interactive/browse_policy_definitions.rs
@@ -1,14 +1,26 @@
+use cloud_terrastodon_azure::prelude::PolicyDefinition;
 use cloud_terrastodon_azure::prelude::fetch_all_policy_definitions;
+use cloud_terrastodon_azure::prelude::Scope;
+use cloud_terrastodon_user_input::Choice;
 use cloud_terrastodon_user_input::PickerTui;
 use itertools::Itertools;
 use tracing::info;
 
 pub async fn browse_policy_definitions() -> eyre::Result<()> {
-    let policy_definitions = fetch_all_policy_definitions().await?;
-    let chosen = PickerTui::new(policy_definitions).pick_many()?;
+    let policy_definitions = fetch_all_policy_definitions()
+        .await?
+        .into_iter()
+        .map(|def| Choice {
+            key: match def.description.as_ref() {
+                Some(desc) => format!("{def} - {desc}"),
+                None => format!("{def} - no description"),
+            },
+            value: def,
+        });
+    let chosen: Vec<PolicyDefinition> = PickerTui::new(policy_definitions).pick_many()?;
     let msg = format!(
         "You chose:\n{}",
-        chosen.iter().map(|x| format!("- {x:#}")).join("\n")
+        chosen.iter().map(|x| format!("- {}", x.id.expanded_form())).join("\n")
     );
     info!("{msg}");
 
