414e81b84da706826d8c415eed2a79c49fb5f674
TeamDman <TeamDman9201@gmail.com>
2025-10-27 13:54:50 -0400

    update tracing for fetch_all_policy_definitions

--- crates/azure/src/policy_definitions.rs
+++ crates/azure/src/policy_definitions.rs
@@ -4,8 +4,14 @@ use cloud_terrastodon_command::CacheBehaviour;
 use eyre::Result;
 use std::path::PathBuf;
 use std::time::Duration;
+use tracing::debug;
 
 pub async fn fetch_all_policy_definitions() -> Result<Vec<PolicyDefinition>> {
+    debug!(
+        event = "fetching",
+        what = "microsoft.authorization/policydefinitions",
+        how = "resource graph",
+    );
     let mut qb = ResourceGraphHelper::new(
         r#"
 policyresources
@@ -26,7 +32,14 @@ policyresources
             valid_for: Duration::from_hours(8),
         },
     );
-    qb.collect_all().await
+    let rtn = qb.collect_all().await?;
+    debug!(
+        event = "fetch complete",
+        what = "microsoft.authorization/policydefinitions",
+        how = "resource graph",
+        count = rtn.len(),
+    );
+    Ok(rtn)
 }
 #[cfg(test)]
 mod tests {
