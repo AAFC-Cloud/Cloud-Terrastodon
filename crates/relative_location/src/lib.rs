ec4fc8291935affadf90cb51cdf116c80c53c7a1
TeamDman <TeamDman9201@gmail.com>
2026-01-05 12:05:54 -0500

    cargo fmt

--- crates/relative_location/src/lib.rs
+++ crates/relative_location/src/lib.rs
@@ -1,23 +1,23 @@
 //! A helper for converting std::panic::Location to relative paths with displayed.
-//! 
-//! When crossing crate boundaries calling a function annotated with 
-//! 
+//!
+//! When crossing crate boundaries calling a function annotated with
+//!
 //! ```rust
 //! #[track_caller]
 //! ```
-//! 
-//! the value returned by 
-//! 
+//!
+//! the value returned by
+//!
 //! ```rust
 //! std::panic::Location::caller()
 //! ```
-//! 
+//!
 //! uses an absolute path.
-//! 
+//!
 //! This crate strips the path segments that are also present in the path of this crate at build time.
-//! 
-//! This means it probably won't work outside of building Cloud Terrastodon 
-//! 
+//!
+//! This means it probably won't work outside of building Cloud Terrastodon
+//!
 //! ㄟ( ▔, ▔ )ㄏ
 
 use std::panic::Location;
