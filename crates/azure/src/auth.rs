725dce806d32f6636a73dbf24dd1fc93c092aaca
TeamDman <TeamDman9201@gmail.com>
2026-01-08 15:47:20 -0500

    add caching to signed-in-user show

--- crates/azure/src/auth.rs
+++ crates/azure/src/auth.rs
@@ -1,4 +1,7 @@
+use std::path::PathBuf;
+
 use cloud_terrastodon_azure_types::prelude::User;
+use cloud_terrastodon_command::CacheKey;
 use cloud_terrastodon_command::CommandBuilder;
 use cloud_terrastodon_command::CommandKind;
 use eyre::Result;
@@ -7,6 +10,11 @@ use tracing::warn;
 pub async fn fetch_current_user() -> Result<User> {
     CommandBuilder::new(CommandKind::AzureCLI)
         .args(["ad", "signed-in-user", "show"])
+        .cache(CacheKey::new(PathBuf::from_iter([
+            "ad",
+            "signed-in-user",
+            "show",
+        ])))
         .run()
         .await
 }
