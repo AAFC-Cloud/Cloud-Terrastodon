925501f8bafdf9718b76f326fb1aac528a2e2b36
TeamDman <TeamDman9201@gmail.com>
2026-06-22 15:46:20 -0400

    fix oauth2permissiongrantid missing facet transparent

--- crates/azure_types/src/oauth2_permission_grants.rs
+++ crates/azure_types/src/oauth2_permission_grants.rs
@@ -5,6 +5,7 @@ use itertools::Itertools;
 // https://learn.microsoft.com/en-us/graph/api/resources/oauth2permissiongrant?view=graph-rest-1.0
 
 #[derive(Debug, Clone, Eq, PartialEq, facet::Facet)]
+#[facet(transparent)]
 pub struct OAuth2PermissionGrantId(pub String);
 impl std::fmt::Display for OAuth2PermissionGrantId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
