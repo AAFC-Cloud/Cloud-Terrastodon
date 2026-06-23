634135a593c2ad05c20215ac8f9b446801da6f2e
TeamDman <TeamDman9201@gmail.com>
2026-06-23 11:48:37 -0400

    remove REAUTH var after tests

--- test.ps1
+++ test.ps1
@@ -3,3 +3,4 @@ cargo build --tests
 # test with fewer workers to avoid rate limits lol
 $env:CLOUD_TERRASTODON_REAUTH="DENY"
 cargo test --workspace --jobs 2
+Remove-Item Env:CLOUD_TERRASTODON_REAUTH
