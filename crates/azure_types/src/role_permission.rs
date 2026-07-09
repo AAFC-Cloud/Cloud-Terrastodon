4aad10b84d7d85867d528a5e69aac59d1fa11f25
TeamDman <TeamDman9201@gmail.com>
2026-07-09 13:47:39 -0400

    move RolePermissionActionSet to its own file

--- crates/azure_types/src/role_permission.rs
+++ crates/azure_types/src/role_permission.rs
@@ -1,41 +1,6 @@
-use crate::RolePermissionAction;
+use crate::{RolePermissionAction, RolePermissionActionSet};
 use arbitrary::Arbitrary;
-use indexmap::IndexSet;
-use std::{
-    cmp::Ordering,
-    ops::{Deref, DerefMut},
-};
-#[derive(Debug, Default, PartialEq, Eq, Clone, facet::Facet, Arbitrary)]
-#[facet(transparent)]
-pub struct RolePermissionActionSet(IndexSet<RolePermissionAction>);
-impl Deref for RolePermissionActionSet {
-    type Target = IndexSet<RolePermissionAction>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-impl DerefMut for RolePermissionActionSet {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-impl std::hash::Hash for RolePermissionActionSet {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        // sort then hash rather than use the indexset iteration order which depends on insertion order
-        let mut sorted_actions: Vec<_> = self.0.iter().collect();
-        sorted_actions.sort();
-        for action in sorted_actions {
-            action.hash(state);
-        }
-    }
-}
-
-impl FromIterator<RolePermissionAction> for RolePermissionActionSet {
-    fn from_iter<T: IntoIterator<Item = RolePermissionAction>>(iter: T) -> Self {
-        Self(iter.into_iter().collect())
-    }
-}
+use std::cmp::Ordering;
 
 /// See also: `az provider operation list`
 #[derive(Debug, PartialEq, Eq, Clone, Hash, facet::Facet)]
@@ -327,32 +292,5 @@ mod test {
         let perm = RolePermissions::new([], [], [], []);
         assert!(perm.satisfies(&[], &[]));
         Ok(())
-    }
-}
-
-impl IntoIterator for RolePermissionActionSet {
-    type Item = RolePermissionAction;
-    type IntoIter = indexmap::set::IntoIter<RolePermissionAction>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
-}
-
-impl<'a> IntoIterator for &'a RolePermissionActionSet {
-    type Item = &'a RolePermissionAction;
-    type IntoIter = indexmap::set::Iter<'a, RolePermissionAction>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.iter()
-    }
-}
-
-impl<'a> IntoIterator for &'a mut RolePermissionActionSet {
-    type Item = &'a RolePermissionAction;
-    type IntoIter = indexmap::set::Iter<'a, RolePermissionAction>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.iter()
     }
 }
---
+++ crates/azure_types/src/role_permission_action_set.rs
@@ -0,0 +1,66 @@
+use crate::RolePermissionAction;
+use arbitrary::Arbitrary;
+use indexmap::IndexSet;
+use std::ops::{Deref, DerefMut};
+
+#[derive(Debug, Default, PartialEq, Eq, Clone, facet::Facet, Arbitrary)]
+#[facet(transparent)]
+pub struct RolePermissionActionSet(IndexSet<RolePermissionAction>);
+
+impl Deref for RolePermissionActionSet {
+    type Target = IndexSet<RolePermissionAction>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for RolePermissionActionSet {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl std::hash::Hash for RolePermissionActionSet {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        // Sort before hashing so logical equality does not depend on insertion order.
+        let mut sorted_actions: Vec<_> = self.0.iter().collect();
+        sorted_actions.sort();
+        for action in sorted_actions {
+            action.hash(state);
+        }
+    }
+}
+
+impl FromIterator<RolePermissionAction> for RolePermissionActionSet {
+    fn from_iter<T: IntoIterator<Item = RolePermissionAction>>(iter: T) -> Self {
+        Self(iter.into_iter().collect())
+    }
+}
+
+impl IntoIterator for RolePermissionActionSet {
+    type Item = RolePermissionAction;
+    type IntoIter = indexmap::set::IntoIter<RolePermissionAction>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a RolePermissionActionSet {
+    type Item = &'a RolePermissionAction;
+    type IntoIter = indexmap::set::Iter<'a, RolePermissionAction>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a mut RolePermissionActionSet {
+    type Item = &'a RolePermissionAction;
+    type IntoIter = indexmap::set::Iter<'a, RolePermissionAction>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.iter()
+    }
+}
